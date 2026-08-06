e039a2620f01f4caae96349d005665e5572f7603
YeonADPL <hongyi.tan@athenadynamics.com>
2026-08-06 13:50:23 +0800

    Updated Docker Compose, Dependency and .env

--- backend/ec/ec/settings.py
+++ backend/ec/ec/settings.py
@@ -12,16 +12,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a5t(@%8hb$5i^nf@m4tx&u1*eu7@$2u(3_4l^r(r-5hh5z!i*!'
+SECRET_KEY = "django-insecure-a5t(@%8hb$5i^nf@m4tx&u1*eu7@$2u(3_4l^r(r-5hh5z!i*!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,66 +35,75 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'api',
-    'rest_framework',
-    'corsheaders',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "api",
+    "rest_framework",
+    "corsheaders",
     # 'rest_framework_simplejwt',
-    'rest_framework.authtoken',
-    'dj_rest_auth',
+    "rest_framework.authtoken",
+    "dj_rest_auth",
 ]
-
 
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'ec.urls'
+ROOT_URLCONF = "ec.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'ec.wsgi.application'
+WSGI_APPLICATION = "ec.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    "default": {
         "ENGINE": "django.db.backends.postgresql",
-        'NAME': "ecommerce",
-        'USER': "postgres",
-        'PASSWORD': "pg",
-        'HOST': "localhost",
-        'PORT': 5432, 
+        "NAME": env("DATABASE_DB"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+        "HOST": env("DATABASE_URL"),
+        "PORT": env("DATABASE_PORT"),
     }
+    # """
+    # 'default': {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     'NAME': "ecommerce",
+    #     'USER': "postgres",
+    #     'PASSWORD': "pg",
+    #     'HOST': "localhost",
+    #     'PORT': 5432,
+    # }
+    # """
 }
 
 
@@ -100,16 +112,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -117,9 +129,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -129,69 +141,61 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True 
+CORS_ALLOW_CREDENTIALS = True
 
-AUTH_USER_MODEL="api.CustomUser"
+AUTH_USER_MODEL = "api.CustomUser"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    "DEFAULT_AUTHENTICATION_CLASSES": (
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-
+        "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     )
 }
 
 REST_AUTH = {
-    'USE_JWT': True,
-    'JWT_AUTH_COOKIE': 'ec-access-token',
-    'JWT_AUTH_REFRESH_COOKIE': 'ec-refresh-token',
+    "USE_JWT": True,
+    "JWT_AUTH_COOKIE": "ec-access-token",
+    "JWT_AUTH_REFRESH_COOKIE": "ec-refresh-token",
 }
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-#     # "ROTATE_REFRESH_TOKENS": False,
-#     # "BLACKLIST_AFTER_ROTATION": False,
-#     # "UPDATE_LAST_LOGIN": False,
-
-#     # "ALGORITHM": "HS256",
-#     # # "SIGNING_KEY": settings.SECRET_KEY,
-#     # "VERIFYING_KEY": "",
-#     # "AUDIENCE": None,
-#     # "ISSUER": None,
-#     # "JSON_ENCODER": None,
-#     # "JWK_URL": None,
-#     # "LEEWAY": 0,
-
-#     "AUTH_HEADER_TYPES": ("Bearer",),
-#     "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
-#     # "USER_ID_FIELD": "id",
-#     # "USER_ID_CLAIM": "user_id",
-#     # "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
-
-#     # "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
-#     # "TOKEN_TYPE_CLAIM": "token_type",
-#     # "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
-
-#     # "JTI_CLAIM": "jti",
-
-#     # "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
-#     # "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
-#     # "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
-
-#     # "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
-#     # "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
-#     # "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
-#     # "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
-#     # "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
-#     # "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
+    #     # "ROTATE_REFRESH_TOKENS": False,
+    #     # "BLACKLIST_AFTER_ROTATION": False,
+    #     # "UPDATE_LAST_LOGIN": False,
+    #     # "ALGORITHM": "HS256",
+    #     # # "SIGNING_KEY": settings.SECRET_KEY,
+    #     # "VERIFYING_KEY": "",
+    #     # "AUDIENCE": None,
+    #     # "ISSUER": None,
+    #     # "JSON_ENCODER": None,
+    #     # "JWK_URL": None,
+    #     # "LEEWAY": 0,
+    #     "AUTH_HEADER_TYPES": ("Bearer",),
+    #     "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    #     # "USER_ID_FIELD": "id",
+    #     # "USER_ID_CLAIM": "user_id",
+    #     # "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+    #     # "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    #     # "TOKEN_TYPE_CLAIM": "token_type",
+    #     # "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+    #     # "JTI_CLAIM": "jti",
+    #     # "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    #     # "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    #     # "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+    #     # "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+    #     # "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+    #     # "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
+    #     # "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
+    #     # "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
+    #     # "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
-
